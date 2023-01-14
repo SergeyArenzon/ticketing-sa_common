76b7ca7a179884336c911fbaf22c1cb9a20bd358
Sergey Arenzon <sergeyr1991@gmail.com>
2023-01-14 21:20:24 +0200

    add jest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express'
-import 'express-async-errors';
-import { json } from 'body-parser'
-import {currentUserRouter} from './routes/current-user'
-import {signinRouter} from './routes/signin'
-import {signupRouter} from './routes/signup'
-import { signoutRouter} from './routes/signout'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found'
+import {app} from './app'
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-
-
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json())
-app.use(cookieSession({
-    signed: false,
-    secure: true,
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*',async () => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler);
 
 const start = async() => {
     if(!process.env.JWT_KEY) throw new Error("No JWT_KEY foudn") 
