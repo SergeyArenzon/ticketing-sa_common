765ed18f64df1f67abe0e27c902290e7ad1b5af1
Sergey Arenzon <sergeyr1991@gmail.com>
2023-02-15 21:08:45 +0200

    .

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({currentUser}) => {
   console.log(currentUser);
@@ -7,20 +7,10 @@ const LandingPage = ({currentUser}) => {
 
 
 
-LandingPage.getInitialProps = async() => {
-   if(typeof window === 'undefined'){
-     const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
-      headers: {
-        Host: 'ticketing.dev'
-      }
-    });
-     return data
-
-   }else {
-    const {data} = await axios.get('/api/users/currentuser');
-    return data;
-   }
-   return {zain:1}
+LandingPage.getInitialProps = async(context) => {
+ 
+   const { data } = buildClient(context).get('/api/users/currentuser');
+   return data;
 }
 
 export default LandingPage;
