a2dfc7e07448dce92bcf1d3ea3612430ad0101a9
SergeyMySize <sergeya@mysizeid.com>
2023-03-02 18:42:40 +0200

    s

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser'
 import {errorHandler,NotFoundError, currentUser} from "@ticketssa/common";
 import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 
 
@@ -17,6 +18,7 @@ app.use(cookieSession({
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*',async () => {
     throw new NotFoundError()
